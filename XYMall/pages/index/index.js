5d2d2eb2c66b5222ed4cda9034d0ddc93a3f4441
yulong <xiaoyulong001@163.com>
2020-04-14 09:49:40 +0800

    09-首页-轮播图-动态渲染

--- XYMall/pages/index/index.js
+++ XYMall/pages/index/index.js
@@ -4,38 +4,41 @@ const app = getApp()
 
 Page({
   data: {
+    // 轮播图数组
+    swiperList: [],
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
+
+  // 页面开始加载就会触发
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+
+    // 1 发送异步请求，获取轮播图数据
+    var reqTask = wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      data: {},
+      header: {
+        'content-type': 'application/json'
+      },
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: (result) => {
+        console.log("接口请求成功");
+        console.log(result);
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+          swiperList: result.data.message
+        });
+      },
+      fail: () => {
+        console.log("接口请求失败");
+      },
+      complete: () => {
+        // 接口请求成功 或者 失败，都走这里
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    });
   },
   onReady: function () {
 
