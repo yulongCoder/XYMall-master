ec689aea588882f239a544dc238278c07ed23885
yulong <xiaoyulong001@163.com>
2020-04-14 19:36:43 +0800

    16-tabBar分类模块-请求接口和页面布局

--- XYMall/pages/category/index.js
+++ XYMall/pages/category/index.js
@@ -1,3 +1,8 @@
+// 0 引入用来发送请求的方法, 小程序的话路径要补完整；
+import {
+  request
+} from "../../request/index.js";
+
 // pages/category/index.js
 Page({
 
@@ -5,62 +10,41 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 左侧的菜单数据
+    leftMenuList: [],
+    // 右侧的商品数据
+    rightContent: [],
+    // 被点击的左侧的菜单
+    currentIndex: 0,
   },
+  // 接口的返回数据
+  Cates: [],
+
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCategories();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // 获取分类数据
+  getCategories() {
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/categories'
+    }).then(result => {
+      this.Cates = result.data.message;
 
-  },
+      // 构造左侧的大菜单数据
+      let leftMenuList = this.Cates.map(v => v.cat_name);
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+      // 构造右侧的商品数据
+      let rightContent = this.Cates[0].children;
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      this.setData({
+        leftMenuList,
+        rightContent
+      });
+    });
   }
 })
