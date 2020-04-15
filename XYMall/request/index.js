37310ea80dff6d9d783d878a6073ddeea1d183b2
yulong <xiaoyulong001@163.com>
2020-04-15 19:19:00 +0800

    29-添加全局的正在加载中图标效果

--- XYMall/request/index.js
+++ XYMall/request/index.js
@@ -1,5 +1,15 @@
+// 同时发送异步代码的次数
+let ajaxTimes = 0;
 
 export const request = (params) => {
+
+  ajaxTimes++;
+  // 显示加载中 效果
+  wx.showLoading({
+    title: "加载中",
+    mask: true
+  });
+
   // 定义公共的url
   const baseUrl = "https://api-hmugo-web.itheima.net/api/public/v1/";
   return new Promise((resolve, reject) => {
@@ -12,8 +22,14 @@ export const request = (params) => {
       },
       fail: (err) => {
         reject(err);
+      },
+      complete: () => {
+        ajaxTimes--;
+        if (ajaxTimes === 0) {
+          // 关闭正在等待的图标
+          wx.hideLoading();
+        }
       }
     });
   });
 }
-
