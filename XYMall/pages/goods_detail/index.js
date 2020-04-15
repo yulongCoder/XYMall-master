83f26435e8f814e323f6b368fb770efe5a4d3167
yulong <xiaoyulong001@163.com>
2020-04-15 19:48:32 +0800

    30-商品详情-获取数据

--- XYMall/pages/goods_detail/index.js
+++ XYMall/pages/goods_detail/index.js
@@ -1,4 +1,31 @@
-// pages/goods_detail/index.js
+/* 
+1 发送请求获取数据 
+2 点击轮播图 预览大图
+  1 给轮播图绑定点击事件
+  2 调用小程序的api  previewImage 
+3 点击 加入购物车
+  1 先绑定点击事件
+  2 获取缓存中的购物车数据 数组格式 
+  3 先判断 当前的商品是否已经存在于 购物车
+  4 已经存在 修改商品数据  执行购物车数量++ 重新把购物车数组 填充回缓存中
+  5 不存在于购物车的数组中 直接给购物车数组添加一个新元素 新元素 带上 购买数量属性 num  重新把购物车数组 填充回缓存中
+  6 弹出提示
+4 商品收藏
+  1 页面onShow的时候  加载缓存中的商品收藏的数据
+  2 判断当前商品是不是被收藏 
+    1 是 改变页面的图标
+    2 不是 。。
+  3 点击商品收藏按钮 
+    1 判断该商品是否存在于缓存数组中
+    2 已经存在 把该商品删除
+    3 没有存在 把商品添加到收藏数组中 存入到缓存中即可
+ */
+import {
+  request
+} from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
+
+
 Page({
 
   /**
@@ -12,6 +39,24 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const {
+      goods_id
+    } = options;
+
+    this.getGoodsDetail(goods_id);
+  },
+
+  // 获取商品详情数据
+  async getGoodsDetail(goods_id) {
+    const res = await request({
+      url: "goods/detail",
+      data: {
+        goods_id
+      }
+    });
+
+    console.log(res);
+    
 
   },
 
