4663728b69fb4c18439fdf1c1df2905dc6431445
yulong <xiaoyulong001@163.com>
2020-04-15 20:09:57 +0800

    32-商品详情-轮播图动态渲染

--- XYMall/pages/goods_detail/index.js
+++ XYMall/pages/goods_detail/index.js
@@ -32,8 +32,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    goodsObj: {},
+    // 商品是否被收藏
+    isCollect: false
   },
+  // 商品对象
+  GoodsInfo: {},
 
   /**
    * 生命周期函数--监听页面加载
@@ -48,15 +52,33 @@ Page({
 
   // 获取商品详情数据
   async getGoodsDetail(goods_id) {
-    const res = await request({
+    const goodsObj = await request({
       url: "goods/detail",
       data: {
         goods_id
       }
     });
+    this.GoodsInfo = goodsObj;
 
-    console.log(res);
-    
+    // 1 获取缓存中的商品收藏的数组
+    let collect = wx.getStorageSync("collect") || [];
+    // 2 判断当前商品是否被收藏
+    let isCollect = collect.some(v => v.goods_id === this.GoodsInfo.goods_id);
+
+    this.setData({
+      goodsObj: {
+        goods_name: goodsObj.goods_name,
+        goods_price: goodsObj.goods_price,
+        // iphone部分手机 不识别 webp图片格式 
+        // 最好找到后台 让他进行修改 
+        // 临时自己改 确保后台存在 1.webp => 1.jpg 
+        goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics: goodsObj.pics
+      },
+      isCollect
+    })
+
+
 
   },
 
