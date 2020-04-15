236d8d9de1ee96360477df07c2aab1449bacf658
yulong <xiaoyulong001@163.com>
2020-04-15 21:56:15 +0800

    37-商品详情-加入购物车

--- XYMall/pages/goods_detail/index.js
+++ XYMall/pages/goods_detail/index.js
@@ -91,6 +91,31 @@ Page({
     });
   },
 
+  // 点击 加入购物车
+  handleCartAdd() {
+    // 1 获取缓存中的购物车 数组
+    let cart = wx.getStorageSync("cart") || [];
+    // 2 判断 商品对象是否存在于购物车数组中
+    let index = cart.findIndex(v => v.goods_id === this.GoodsInfo.goods_id);
+    if (index === -1) {
+      //3  不存在 第一次添加
+      this.GoodsInfo.num = 1;
+      this.GoodsInfo.checked = true;
+      cart.push(this.GoodsInfo);
+    } else {
+      // 4 已经存在购物车数据 执行 num++
+      cart[index].num++;
+    }
+    // 5 把购物车重新添加回缓存中
+    wx.setStorageSync("cart", cart);
+    // 6 弹窗提示
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      // true 防止用户 手抖 疯狂点击按钮 
+      mask: true
+    });
+  },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
