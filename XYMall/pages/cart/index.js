03f3ce29ca7128dcfc762467df103b8717ee8749
yulong <xiaoyulong001@163.com>
2020-04-15 22:25:02 +0800

    38-购物车-购物车分析&收货按钮样式

--- XYMall/pages/cart/index.js
+++ XYMall/pages/cart/index.js
@@ -1,4 +1,11 @@
-// pages/cart/index.js
+/*
+1 获取用户的收货地址
+  1 绑定点击事件
+  2 调用小程序内置 api  获取用户的收货地址  wx.chooseAddress
+
+*/
+
+
 Page({
 
   /**
@@ -9,58 +16,21 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  // 点击 收货地址
+  handleChooseAddress() {
+    console.log("handleChooseAddress");
+    // 2 获取收货地址
+    wx.chooseAddress({
+      success: (result) => {
+        console.log(result);
+      }
+    });
 
   }
 })
