495f864ec8d4973f840c99c7d73082b21b79d76a
yulong <xiaoyulong001@163.com>
2020-04-17 20:49:18 +0800

    66-订单查询-引入tabs组件

--- XYMall/pages/order/index.js
+++ XYMall/pages/order/index.js
@@ -5,7 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs: [{
+        id: 0,
+        value: "全部",
+        isActive: true
+      },
+      {
+        id: 1,
+        value: "待付款",
+        isActive: false
+      },
+      {
+        id: 2,
+        value: "待发货",
+        isActive: false
+      },
+      {
+        id: 3,
+        value: "退款/退货",
+        isActive: false
+      }
+    ]
   },
 
   /**
@@ -62,5 +82,28 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  // 根据标题索引来激活选中 标题数组
+  changeTitleByIndex(index) {
+    // 2 修改源数组
+    let {
+      tabs
+    } = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+    // 3 赋值到data中
+    this.setData({
+      tabs
+    })
+  },
+
+  handleTabsItemChange(e) {
+    // 1 获取被点击的标题索引
+    const {
+      index
+    } = e.detail;
+    this.changeTitleByIndex(index);
+    // 2 重新发送请求 type=1 index=0
+    // this.getOrders(index + 1);
   }
 })
