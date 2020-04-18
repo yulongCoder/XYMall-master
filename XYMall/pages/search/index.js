800536be716b1581862165feb904812a2978a736
yulong <xiaoyulong001@163.com>
2020-04-18 19:51:26 +0800

    76-搜索中心-渲染搜索结果

--- XYMall/pages/search/index.js
+++ XYMall/pages/search/index.js
@@ -10,18 +10,50 @@
   1 定义全局的定时器id
  */
 
+import {
+  request
+} from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goods:[]
   },
 
   // 输入框的值改变 就会触发的事件
   handleInput(e) {
     console.log(e.detail.value);
+
+    // 1 获取输入框的值
+    const {
+      value
+    } = e.detail;
+    // 2 检测合法性
+    if (!value.trim()) {
+      // 值不合法
+      return;
+    }
+
+    // 3 准备发送请求获取数据
+    this.qsearch(value);
+  },
+
+  // 发送请求获取搜索建议 数据
+  async qsearch(query) {
+    const res = await request({
+      url: "goods/qsearch",
+      data: {
+        query
+      }
+    });
+    console.log(res);
+    this.setData({
+      goods: res
+    })
   }
-  
+
 })
