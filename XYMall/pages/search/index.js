90798ba15552cc13fe2207b6acfb72c28f854157
yulong <xiaoyulong001@163.com>
2020-04-18 21:55:56 +0800

    78-搜索中心-页面痕迹重置

--- XYMall/pages/search/index.js
+++ XYMall/pages/search/index.js
@@ -22,7 +22,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    goods: []
+    goods: [],
+    // 取消 按钮 是否显示
+    isFocus:false,
+    // 输入框的值
+    inpValue:""
   },
 
   TimeId: -1,
@@ -37,11 +41,18 @@ Page({
     } = e.detail;
     // 2 检测合法性
     if (!value.trim()) {
+      this.setData({
+        goods:[],
+        isFocus:false
+      })
       // 值不合法
       return;
     }
 
     // 3 准备发送请求获取数据
+    this.setData({
+      isFocus:true
+    });
     clearTimeout(this.TimeId);
     this.TimeId = setTimeout(() => {
       this.qsearch(value);
@@ -59,6 +70,15 @@ Page({
     console.log(res);
     this.setData({
       goods: res
+    })
+  },
+
+  // 点击 取消按钮
+  handleCancel() {
+    this.setData({
+      inpValue: "",
+      isFocus: false,
+      goods: []
     })
   }
 
