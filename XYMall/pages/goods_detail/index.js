5550bb35455608864f7930308482be4800e27e79
yulong <xiaoyulong001@163.com>
2020-04-18 17:13:31 +0800

    71-商品详情-点击图标切换收藏

--- XYMall/pages/goods_detail/index.js
+++ XYMall/pages/goods_detail/index.js
@@ -10,6 +10,7 @@
   4 已经存在 修改商品数据  执行购物车数量++ 重新把购物车数组 填充回缓存中
   5 不存在于购物车的数组中 直接给购物车数组添加一个新元素 新元素 带上 购买数量属性 num  重新把购物车数组 填充回缓存中
   6 弹出提示
+
 4 商品收藏
   1 页面onShow的时候  加载缓存中的商品收藏的数据
   2 判断当前商品是不是被收藏 
@@ -40,13 +41,15 @@ Page({
   GoodsInfo: {},
 
   /**
-   * 生命周期函数--监听页面加载
+   * 生命周期函数--监听页面显示
    */
-  onLoad: function (options) {
+  onShow: function () {
+    let pages = getCurrentPages();
+    let currentPage = pages[pages.length - 1];
+    let options = currentPage.options;
     const {
       goods_id
     } = options;
-
     this.getGoodsDetail(goods_id);
   },
 
@@ -60,7 +63,7 @@ Page({
     });
     this.GoodsInfo = goodsObj;
 
-    // 1 获取缓存中的商品收藏的数组
+    // 1 获取缓存中的商品收藏的数组 
     let collect = wx.getStorageSync("collect") || [];
     // 2 判断当前商品是否被收藏
     let isCollect = collect.some(v => v.goods_id === this.GoodsInfo.goods_id);
@@ -117,17 +120,45 @@ Page({
     });
   },
 
+  // 点击 商品收藏图标
+  handleCollect() {
+    let isCollect = false;
+    // 1 获取缓存中的商品收藏数组
+    let collect = wx.getStorageSync("collect") || [];
+    // 2 判断该商品是否被收藏过
+    let index = collect.findIndex(v => v.goods_id === this.GoodsInfo.goods_id);
+    // 3 当index！=-1表示 已经收藏过 
+    if (index !== -1) {
+      // 能找到 已经收藏过了  在数组中删除该商品
+      collect.splice(index, 1);
+      isCollect = false;
+      wx.showToast({
+        title: '取消成功',
+        icon: 'success',
+        mask: true
+      });
+    } else {
+      // 没有收藏过
+      collect.push(this.GoodsInfo);
+      isCollect = true;
+      wx.showToast({
+        title: '收藏成功',
+        icon: 'success',
+        mask: true
+      });
+    }
+    // 4 把数组存入到缓存中
+    wx.setStorageSync("collect", collect);
+    // 5 修改data中的属性  isCollect
+    this.setData({
+      isCollect
+    });
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
 
   },
 
